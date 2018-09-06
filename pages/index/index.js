fc3e9c5499f92e7856e9015e2532827ca9d9cbc0
kean <keanli1996@qq.com>
2018-09-06 17:34:06 +0800

    The homepage is set up, but it cannot be displayed on the phone.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,4 @@
 var TYPEINDEX = 0 // 新闻种类在this.data.newsTabs数组中的位置
-var LISTNUM = 7 //加载新闻列表的数量
 Page({
   data:{
     newsTabs: [
@@ -15,58 +14,58 @@ Page({
     top:{},
     newsList:[]
   },
+
   onLoad(){
     this.getLatestNews(TYPEINDEX);
   },
+
   getLatestNews(typeIndex, callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list', 
       data: { "type": this.data.newsTabs[typeIndex].type },
       success: res=>{
         let result=res.data.result
-        this.setNewsData(result,LISTNUM)
+        this.setNewsData(result)
       }
     })
   },
-    setNewsData(result,listNum){
-      let newsList=[]
-      let listLength=0
-      if(listNum > result.length){
-        listLength = result.length
-      }
-      else{listLength = listNum}
-      for(var i=1 ; i<=listLength ; ++i){
-          let temp=result[i]
-          temp.date = temp.date.slice(0,10)
-          newsList.push(temp)
-      }
-      let top = {}
-      top = result[0]
-      top.date = top.date.slice(0, 10)
-      console.log(top)
-      this.setData({
-        top:top,
-        newsList:newsList
-      })
-    },
-    onNewsTabsTap(event) {
-      LISTNUM = 7
-      TYPEINDEX = event.currentTarget.dataset.type
-      this.setNewsTabsStyle()
-      this.getLatestNews(TYPEINDEX, LISTNUM)
-    },
-    setNewsTabsStyle() {
-      let newsTabs = []
-      for (let i = 0; i < 7; i++) {
-      let temp = this.data.newsTabs[i]
-      temp.select = ''
-      temp.selectLine = ''
-      newsTabs.push(temp)
-      }
-      newsTabs[TYPEINDEX].select = 'selected'
-      newsTabs[TYPEINDEX].selectLine = 'selected-line'
-      this.setData({
-        newsTabs: newsTabs
-      })
-    },
+
+  setNewsData(result){
+    let newsList=[]
+    let listLength=0  
+    listLength = result.length
+    for(var i=1 ; i<listLength ; ++i){
+        let temp=result[i]
+        temp.date = temp.date.slice(11,16)
+        newsList.push(temp)
+    }
+    let top = {}
+    top = result[0]
+    top.date = top.date.slice(11, 16)
+    this.setData({
+      top:top,
+      newsList:newsList
+    })
+  },
+
+  onNewsTabsTap(event) {
+    TYPEINDEX = event.currentTarget.dataset.type
+    this.setNewsTabsStyle()
+    this.getLatestNews(TYPEINDEX)
+  },
+
+  setNewsTabsStyle() {
+    let newsTabs = []
+    for (let i = 0; i < 7; i++) {
+    let temp = this.data.newsTabs[i]
+    temp.select = '' 
+    temp.selectLine = ''
+    newsTabs.push(temp)
+    }
+    newsTabs[TYPEINDEX].select = 'selected'
+    newsTabs[TYPEINDEX].selectLine = 'selected-line'
+    this.setData({
+      newsTabs: newsTabs
+    })
+  },
 })
